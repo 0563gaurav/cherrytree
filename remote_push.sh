2fdd7fe1ad3a297cc0cb66b2fee4821f4047c99d
Gaurav Gautam Shakya <Gauravshakya4030@gmail.com>
2022-12-19 22:38:38 +0530

    updated Mon Dec 19 10:38:38 PM IST 2022

--- remote_push.sh
+++ remote_push.sh
@@ -1,5 +1,5 @@
-git add /home/gaurav/grvDoc/cherrytree/*
-git add /home/gaurav/grvDoc/cherrytree/openstack/*
-git add /home/gaurav/grvDoc/cherrytree/pdf/*
+git add /home/gaurav/grvDoc/cherrytree/.
+git add /home/gaurav/grvDoc/cherrytree/openstack/.
+git add /home/gaurav/grvDoc/cherrytree/pdf/.
 git commit -m " updated $(date)"
 git push 
