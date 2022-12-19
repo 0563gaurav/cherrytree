9fa52e6b301b00c92fe007638211af5f83c3832a
Gaurav Gautam Shakya <Gauravshakya4030@gmail.com>
2022-12-19 22:45:20 +0530

    updated Mon Dec 19 10:45:20 PM IST 2022

--- remote_push.sh
+++ remote_push.sh
@@ -3,8 +3,9 @@ git add /home/gaurav/grvDoc/cherrytree/openstack/.
 git add /home/gaurav/grvDoc/cherrytree/pdf/.
 git commit -m " updated $(date)"
 echo "Want To push ?"
+boolean=""
 read boolean
-if boolean == "yes"
+if $boolean == "yes"
 then
 	git push
 fi
